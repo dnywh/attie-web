7f9a760ae195f97e0f5b87a66ae9ab6e3d17424c
Danny White <3104761+dnywh@users.noreply.github.com>
2024-10-28 20:03:52 +0100

    Grouped matches in reverse order

--- src/pages/api/matches.js
+++ src/pages/api/matches.js
@@ -5,7 +5,7 @@ const authToken = await import.meta.env.FD_AUTH_TOKEN
 // https://api.football-data.org/v4/competitions/PL/matches?season=2024&dateFrom=2024-10-26&dateTo=2024-10-27
 
 export async function get(context) {
-    const response = await fetch(`https://api.football-data.org/v4/competitions/PL/matches?season=2024`, {
+    const response = await fetch(`https://api.football-data.org/v4/competitions/PL/matches?season=2024&dateFrom=2024-08-26&dateTo=2024-11-27`, {
         headers: {
             'X-Auth-Token': authToken
         }
@@ -13,5 +13,27 @@ export async function get(context) {
 
     const data = await response.json();
     // console.log(data.matches)
-    return data.matches;
+    // return data.matches;
+
+    const matches = data.matches.sort((a, b) => new Date(b.utcDate).getTime() - new Date(a.utcDate).getTime());
+
+    const groupedMatches = {};
+    matches.forEach(match => {
+        const localDate = new Date(match.utcDate).toLocaleDateString();
+        if (!groupedMatches[localDate]) {
+            groupedMatches[localDate] = [];
+        }
+        groupedMatches[localDate].push(match);
+    });
+
+    // console.log(groupedMatches)
+
+    // 
+    return {
+        // body: JSON.stringify(groupedMatches) // Return grouped matches
+        body: groupedMatches,
+        headers: {
+            'Cache-Control': 'public, max-age=300' // Cache for 5 minutes (300 seconds)
+        }
+    };
 }
