e4816c161a7be791a7447c891ccef07b52296de5
Danny White <3104761+dnywh@users.noreply.github.com>
2024-10-28 21:19:48 +0100

    Fix TypeScript errors

--- src/pages/api/matches.js
+++ src/pages/api/matches.js
@@ -4,7 +4,7 @@ const authToken = await import.meta.env.FD_AUTH_TOKEN
 // https://api.football-data.org/v4/competitions/PL/matches?season=2024
 // https://api.football-data.org/v4/competitions/PL/matches?season=2024&dateFrom=2024-10-26&dateTo=2024-10-27
 
-export async function get(context) {
+export async function get() {
     const response = await fetch(`https://api.football-data.org/v4/competitions/PL/matches?season=2024&dateFrom=2024-08-26&dateTo=2024-11-27`, {
         headers: {
             'X-Auth-Token': authToken
