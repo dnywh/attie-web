c742250e861c8070cdb28be9011a7171645f79a3
Danny White <3104761+dnywh@users.noreply.github.com>
2024-11-02 10:51:14 +0100

    Local match data for testing

--- src/pages/api/matches.js
+++ src/pages/api/matches.js
@@ -4,21 +4,29 @@ const authToken = await import.meta.env.FD_AUTH_TOKEN
 // https://api.football-data.org/v4/competitions/PL/matches?season=2024
 // https://api.football-data.org/v4/competitions/PL/matches?season=2024&dateFrom=2024-10-26&dateTo=2024-10-27
 
-export async function get() {
-    const response = await fetch(`https://api.football-data.org/v4/competitions/PL/matches?season=2024&dateFrom=2024-08-26&dateTo=2024-11-27`, {
-        headers: {
-            'X-Auth-Token': authToken
-        }
-    });
 
-    const data = await response.json();
-    // console.log(data.matches)
-    // return data.matches;
+export async function getMatchData() {
+    const uselocalData = true;
+    let matches;
 
-    const matches = data.matches.sort((a, b) => new Date(b.utcDate).getTime() - new Date(a.utcDate).getTime());
+    if (uselocalData) {
+        const localData = await import('./data/local_matches.json');
+        console.log("Using local data")
+        matches = localData.default.matches;
+    } else {
+        const response = await fetch(`https://api.football-data.org/v4/competitions/PL/matches?season=2024&dateFrom=2024-08-26&dateTo=2024-11-27`, {
+            headers: {
+                'X-Auth-Token': authToken
+            }
+        });
+        const data = await response.json();
+        matches = data.matches;
+    }
+
+    const sortedMatches = matches.sort((a, b) => new Date(b.utcDate).getTime() - new Date(a.utcDate).getTime());
 
     const groupedMatches = {};
-    matches.forEach(match => {
+    sortedMatches.forEach(match => {
         const localDate = new Date(match.utcDate).toLocaleDateString();
         if (!groupedMatches[localDate]) {
             groupedMatches[localDate] = [];
@@ -26,11 +34,7 @@ export async function get() {
         groupedMatches[localDate].push(match);
     });
 
-    // console.log(groupedMatches)
-
-    // 
     return {
-        // body: JSON.stringify(groupedMatches) // Return grouped matches
         body: groupedMatches,
         headers: {
             'Cache-Control': 'public, max-age=300' // Cache for 5 minutes (300 seconds)
